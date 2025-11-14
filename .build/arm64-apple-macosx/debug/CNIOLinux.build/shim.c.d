082dd216bd62d0e65741c0fef5e2a756ad4e6315
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-11-14 06:59:51 +0100

    chore: add .gitignore; untrack .build artifacts

--- .build/arm64-apple-macosx/debug/CNIOLinux.build/shim.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.5.sdk/usr/include/Darwin.modulemap \
-  /Users/benedikteickhoff/Development/Github-Desktop/FountainKit/Sidecar/FountainCoreMIDIBridge/.build/checkouts/swift-nio/Sources/CNIOLinux/shim.c
--- .build/arm64-apple-macosx/debug/CNIOWindows.build/WSAStartup.c.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/benedikteickhoff/Development/Github-Desktop/FountainKit/Sidecar/FountainCoreMIDIBridge/.build/checkouts/swift-nio/Sources/CNIOWindows/include/module.modulemap \
-  /Users/benedikteickhoff/Development/Github-Desktop/FountainKit/Sidecar/FountainCoreMIDIBridge/.build/checkouts/swift-nio/Sources/CNIOWindows/WSAStartup.c
